d3a0f0f0eec0e5c426f211c27e6ad571dca5eb8b
Jagruti Pancham <jagruti.pancham@corp.ola.com>
2022-08-03 12:36:17 +0530

    simplified version of all

---
+++ pages/Login.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="px-8 py-6 mt-4 text-left bg-gray-400 shadow-lg">
+        <p class="text-2xl font-bold m-5 ">
+            Login
+        </p>
+        <div>
+            <input v-model="username" type="text" placeholder="User Name" class="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2" />
+            <input v-model="password" type="password" placeholder="Password" class="text-sm text-gray-base w-full mr-3py-5 px-4 h-2 border border-gray-200 rounded mb-2" />
+           
+            <div class="flex justify-center border-black">
+                <button @click="login" class="shadow bg-purple-100 hover:bg-grey-1000 focus:shadow-outline focus:outline-none text-black border-black font-bold py-2 px-4 rounded">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    name: "login",
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    methods: {
+        async login() {
+            let result = await axios.get(
+                `http://localhost:3000/users?username=${this.username}&password=${this.password}`
+            );
+            if (result.status == 200 && result.data.length > 0) {
+                this.$router.push({
+                    name: "bucket"
+                })
+            } else {
+                alert("please enter correct username and password")
+            }
+        }
+    }
+}
+</script>
---
+++ pages/signUp.vue
@@ -0,0 +1,73 @@
+<template>
+<div flex items-center justify-center min-h-screen bg-gray-100>
+    <h1>Register</h1>
+    <form method="post">
+        <input type="text" placeholder="User name" v-model="username" id="name"><br>
+
+        <input type="password" placeholder="Password" v-model="password" id="password"><br>
+
+        <button type="submit" @click="signUp"><NuxtLink to="Login">Submit</NuxtLink></button>
+    </form>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:"sign",
+    data()
+    {
+        return {
+            username:'',
+            password:'',
+        }
+    },
+    methods:{
+        async signUp(){
+            console.warn("this is called",this.username,this.password)
+            let result= await axios.post("http://localhost:3000/users",
+                {
+                    username:this.username,
+                    password:this.password
+                })
+            console.warn(result)
+            if(result.status==201){
+                alert("You are registerd!")
+            }
+}
+    }
+}
+</script>
+
+<style scoped>
+div {
+    text-align: center;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0px;
+    background-color:white;
+}
+
+input[type=text],
+input[type=password] {
+    background-color: rgb(241, 245, 247);
+    width: 20%;
+    padding: 12px 20px;
+    border-radius: 4px;
+    margin: 8px 2px;
+    display: inline-block;
+    border: 1px solid rgb(17, 17, 17);
+    box-sizing: border-box;
+}
+
+button {
+    background-color: skyblue;
+    color: white;
+    padding: 14px;
+    margin: 2px;
+    border: 1px solid rgb(17, 17, 17);
+    cursor: pointer;
+    width: 20%;
+
+}
+</style>
